d4816451f218024c3217543bb1c7ba73cfbfd027
Alessandro <univale9966aa@gmail.com>
2022-04-11 10:55:33 +0200

    Aggiunti metodi in service e app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from './employee';
+import { EmployeeService } from './employee.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'employeemanagerapp';
+
+  //Initialized an array of Employee; need to initialize it to empty, otherwise i'll get an error!
+  public employees: Employee[] = [];
+
+  constructor(private employeeService: EmployeeService) { }
+
+  ngOnInit(){
+    this.getEmployees();
+  }
+
+  public getEmployees(): void {
+    this.employeeService.getEmployees().subscribe(
+      (response: Employee[]) => {
+        this.employees = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
 }
