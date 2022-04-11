7b85ff0a35ed8cde7a2878be9e34826cf54988bc
Alessandro <univale9966aa@gmail.com>
2022-04-11 11:48:23 +0200

    Preso snippet e importato CSS + Bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -9,7 +10,9 @@ import { EmployeeService } from './employee.service';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    //Need to import this module to use HttpClientModule
+    HttpClientModule
   ],
   providers: [EmployeeService],
   bootstrap: [AppComponent]
